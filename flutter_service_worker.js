d739c66151c10b170303b51e2f3d5c95540ef294
Carlos Vélez <80561943+carlsevels@users.noreply.github.com>
2026-02-09 23:44:16 -0600

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "06362f626753bb98a0fe69e4edf9f6a2",
+const RESOURCES = {"flutter_bootstrap.js": "568ad9abb07ba10713850b63de923ed0",
 "version.json": "c1a4d9568c3b9df80ff931621dfbb9a7",
 "index.html": "72be29968689788d77dca2ac2f1f988e",
 "/": "72be29968689788d77dca2ac2f1f988e",
